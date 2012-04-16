4457af719e1426e59a59a06cbe58afab613c0117
Dave Abrahams <dave@boostpro.com>
2012-04-15 21:27:03 -0400

    clean up copyright

--- Modules/RypplExport.cmake
+++ Modules/RypplExport.cmake
@@ -1,9 +1,9 @@
 ##########################################################################
-# Copyright (C) 2011 Daniel Pfeifer <daniel@pfeifer-mail.de> #
-# #
-# Distributed under the Boost Software License, Version 1.0. #
-# See accompanying file LICENSE_1_0.txt or copy at #
-# http://www.boost.org/LICENSE_1_0.txt #
+# Copyright (C) 2011-2012 Daniel Pfeifer <daniel@pfeifer-mail.de>        #
+#                                                                        #
+# Distributed under the Boost Software License, Version 1.0.             #
+# See accompanying file LICENSE_1_0.txt or copy at                       #
+# http://www.boost.org/LICENSE_1_0.txt                                   #
 ##########################################################################
 
 include(CMakeParseArguments)
