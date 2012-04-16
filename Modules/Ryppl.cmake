24e58419234f2a9a214373ab91f8012f05369562
Dave Abrahams <dave@boostpro.com>
2012-04-16 07:09:03 -0400

    Use a macro to make sure find_package gets called in the right scope

--- Modules/Ryppl.cmake
+++ Modules/Ryppl.cmake
@@ -3,21 +3,13 @@ if(__RYPPL_INCLUDED)
 endif()
 set(__RYPPL_INCLUDED TRUE)
 
-function(find_package)
-  _find_package(${ARGV})
+function(ryppl_do_find_package)
+  set_property(DIRECTORY APPEND PROPERTY RYPPL_FIND_PACKAGE_ARGS "${ARGV}")
+endfunction(ryppl_do_find_package)
 
-  get_cmake_property(variables VARIABLES)
-  foreach(variable ${variables})
-    set(${variable} ${${variable}} PARENT_SCOPE)
-  endforeach()
-
-  get_directory_property(find_package_args RYPPL_FIND_PACKAGE_ARGS)
-
-  if (find_package_args)
-    set(find_package_args ${find_package_args} ";" ${ARGV})
-  else()
-    set(find_package_args ${ARGN})
-  endif()
-endfunction(find_package)
+macro(ryppl_find_package)
+  find_package(${ARGV})
+  ryppl_do_find_package(${ARGV})
+endmacro(ryppl_find_package)
 
 
