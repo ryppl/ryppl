1d2dd6daf5e0767546246e92eae5c07d6724c252
Dave Abrahams <dave@boostpro.com>
2012-04-16 00:15:20 -0400

    Try a horrible find_package macro trick

--- Modules/Ryppl.cmake
+++ Modules/Ryppl.cmake
@@ -1,25 +1,26 @@
-get_property(ryppl_included GLOBAL PROPERTY RYPPL_INCLUDED DEFINED)
-if(NOT ${ryppl_included})
-
-  define_property(GLOBAL PROPERTY RYPPL_INCLUDED
-    BRIEF_DOCS "Functions as an include() guard at the CMake level"
-    FULL_DOCS "Prevents us from overriding the built-in find_package function twice"
-    "See http://www.cmake.org/pipermail/cmake/2011-March/043320.html for more information"
-    )
-
-  include(CMakeParseArguments)
-
-  function(find_package)
-    get_directory_property(find_package_args RYPPL_FIND_PACKAGE_ARGS)
-
-    if (find_package_args)
-      set(find_package_args ${find_package_args} ";" ${ARGN})
-    else()
-      set(find_package_args ${ARGN})
-    endif()
-
-    set_property(DIRECTORY PROPERTY RYPPL_FIND_PACKAGE_ARGS ${find_package_args})
-    message(STATUS "${PROJECT_NAME}.RYPPL_FIND_PACKAGE_ARGS: ${find_package_args}")
-    _find_package(${ARGN})
-  endfunction(find_package)
+if(__RYPPL_INCLUDED)
+  return()
 endif()
+set(__RYPPL_INCLUDED TRUE)
+
+# Causes the built-in find_package to be renamed _find_package
+function(find_package)
+endfunction(find_package)
+
+function(ryppl_find_package)
+  get_directory_property(find_package_args RYPPL_FIND_PACKAGE_ARGS)
+
+  if (find_package_args)
+    set(find_package_args ${find_package_args} ";" ${ARGN})
+  else()
+    set(find_package_args ${ARGN})
+  endif()
+
+  set_property(DIRECTORY PROPERTY RYPPL_FIND_PACKAGE_ARGS ${find_package_args})
+  message(STATUS "${PROJECT_NAME}.RYPPL_FIND_PACKAGE_ARGS: ${find_package_args}")
+endfunction(ryppl_find_package)
+
+macro(find_package)
+  ryppl_find_package(${ARGV})
+  _find_package(${ARGV})
+endmacro(find_package)
