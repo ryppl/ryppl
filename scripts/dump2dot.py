8f467e03614f833aab9745c529522667154ab4a7
Dave Abrahams <dave@boostpro.com>
2012-04-30 17:41:42 -0400

    Refine dotfile procedure

--- scripts/dump2dot.py
+++ scripts/dump2dot.py
@@ -12,14 +12,37 @@ from read_dumps import read_dumps
 
 def run(dump_dir=None):
     all_dumps = read_dumps(dump_dir)
+
+    from SCC import SCC
+    def successors(v):
+        return [
+            lib for lib in (
+                fp.findtext('arg') for fp
+                in (all_dumps.get(v, Element('x')).findall('find-package')
+                + all_dumps.get(v, Element('x')).findall('find-package-indirect'))
+                )]
+
+    sccs = SCC(lambda x:x, successors).getsccs(all_dumps)
+    import pprint
+    cyclic = set()
+    for scc in sccs:
+        if len(scc) > 1:
+            cyclic |= set(scc)
+
     print 'digraph boost {'
-    for s, dump in all_dumps.items():
+    print 'layout=neato;'
+    print 'overlap=scalexy;'
+    print 'splines=true;'
+    for s in cyclic:
+        dump = all_dumps[s]
         if dump.find('libraries/library') is not None:
             print s, '[shape=box3d]'
-        for t in dump.findall('find-package'):
-            print s,'->', t.find('arg').text
-        # for t in dump.findall('find-package-indirect'):
-        #     print s,'->', t.find('arg').text,'[style=dotted]'
+        for t in [t.find('arg').text for t in dump.findall('find-package')]:
+            if t in cyclic:
+                print s,'->', t
+        for t in dump.findall('find-package-indirect'):
+            if t in cyclic:
+                print s,'->', t.find('arg').text,'[style=dotted]'
     print '}'
 
 if __name__ == '__main__':
