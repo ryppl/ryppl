4a220df3a5e97b48f17927c955ff847b11e2f13a
Dave Abrahams <dave@boostpro.com>
2012-04-30 22:30:45 -0400

    Refinement of graph dumping

--- scripts/dump2dot.py
+++ scripts/dump2dot.py
@@ -10,16 +10,46 @@ from xml.etree.cElementTree import ElementTree, Element
 from path import Path
 from read_dumps import read_dumps
 
+def direct_successors(all_dumps, v):
+    return set( fp.findtext('arg') for fp in all_dumps.get(v, Element('x')).findall('find-package') )
+
+def indirect_successors(all_dumps, v):
+    return set( fp.findtext('arg') for fp in all_dumps.get(v, Element('x')).findall('find-package-indirect') )
+
+def usage_dependencies(all_dumps, v):
+    return set( d.text for d in all_dumps.get(v, Element('x')).findall('depends/dependency') )
+
+def usage_successors(all_dumps, v):
+    succ = set()
+    for s in direct_successors(all_dumps, v):
+        succ |= set(x.text for x in all_dumps.get(v, Element('x')).findall('depends/dependency'))
+    return succ
+
+def successors(all_dumps, v):
+    return direct_successors(all_dumps,v) | indirect_successors(all_dumps,v)
+
 def run(dump_dir=None):
     all_dumps = read_dumps(dump_dir)
     print 'digraph boost {'
+    print 'splines=true;'
+    print 'layout=dot;'
+    #print 'nodesep=3;'
+    print 'overlap=scalexy;'
     for s, dump in all_dumps.items():
         if dump.find('libraries/library') is not None:
             print s, '[shape=box3d]'
-        for t in dump.findall('find-package'):
-            print s,'->', t.find('arg').text
-        # for t in dump.findall('find-package-indirect'):
-        #     print s,'->', t.find('arg').text,'[style=dotted]'
+
+        for t in direct_successors(all_dumps,s):
+            if successors(all_dumps, t):
+                print s,'->', t,'[style=bold]'
+
+        for t in indirect_successors(all_dumps, s):
+            if successors(all_dumps, t):
+                print s,'->', t,'[style=dotted,arrowhead=open,color=gray]'
+
+        for t in usage_successors(all_dumps, s):
+            if usage_successors(all_dumps, t):
+                print s,'->', t,'[style=dashed,arrowhead=open,color=blue]'
     print '}'
 
 if __name__ == '__main__':
