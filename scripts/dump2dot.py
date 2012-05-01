7f2ed52359d92fce1d9a8d718c1ce0b1bf1f472a
Dave Abrahams <dave@boostpro.com>
2012-05-01 10:54:15 -0400

    Finally, a graph with useful information
    - Leaves are omitted
    
    - vertices that participate in cycles are colored according to their
      strongly-connected component (SCC)
    
    - direct dependencies are drawn in bold
    
    - Edges in the transitive reduction of the direct+indirect graph that
      aren't in the direct graph are colored blue and dashed
    
    - see http://cl.ly/2M2e2q1j3Q183G252G1G/boost-deps.svg for an example
    
    - I don't understand the origin of the self-loop on Boost.Graph

--- scripts/dump2dot.py
+++ scripts/dump2dot.py
@@ -28,27 +28,84 @@ def usage_successors(all_dumps, v):
 def successors(all_dumps, v):
     return direct_successors(all_dumps,v) | indirect_successors(all_dumps,v)
 
+colors=('red','green','orange', 'blue', 'indigo', 'violet')
+
+def first(seq):
+    return iter(seq).next()
+
 def run(dump_dir=None):
     all_dumps = read_dumps(dump_dir)
+    g = all_dumps
+
+    # find all strongly-connected components
+    from SCC import SCC
+    sccs = SCC(str, lambda i: successors(g, i)).getsccs(g)
+
+    # map each vertex to a scc set
+    scc = {}
+    for component in sccs:
+        s = set(component)
+        for u in s:
+            scc[u] = s
+
+    long_sccs = [s for s in sccs if len(s) > 1]
+
+    # color each vertex in an SCC of size > 1 according to its SCC
+    color = {}
+    for i,s in enumerate(long_sccs):
+        for u in s:
+            color[u] = colors[i]
+
+    V = set(u for u in g if successors(g,u))
+    t_redux = dict((s, set(t for t in successors(g, s) if t in V)) for s in V)
+
+    for component in sccs:
+        representative = first(component)
+        if representative not in V:
+            continue
+        
+        # Keep only one representative link to each other component
+        visited = set()
+        scc_s = scc[representative]
+
+        for s in component:
+            succs = t_redux[s]
+            for t in list(succs):
+                scc_t = scc[t]
+                if id(scc_t) in visited:
+                    succs.remove(t)
+                elif id(scc_t) != id(scc_s):
+                    visited.add(id(scc_t))
+                
+        N = len(scc_s)
+        if N > 1:
+            # replace SCCs with simple cycles
+            next = dict((component[i], component[(i+1)%N]) for i in range(N))
+            for u in component:
+                t_redux[u] -= set(v for v in component if v != next[u])
+                t_redux[u].add(next[u])
+    
+    direct = dict((s, set(t for t in direct_successors(g, s) if t in V)) for s in V)
+
     print 'digraph boost {'
     print 'splines=true;'
     print 'layout=dot;'
     #print 'nodesep=3;'
     print 'overlap=scalexy;'
-    for s, dump in all_dumps.items():
-        if dump.find('libraries/library') is not None:
-            print s, '[shape=box3d]'
+    for s in V:
+        c = color.get(s,'black')
 
-        for t in direct_successors(all_dumps,s):
-            if successors(all_dumps, t):
-                print s,'->', t,'[style=bold]'
+        if all_dumps[s].find('libraries/library') is not None:
+            print s, '[shape=box3d,color=%s]'%c
+        else:
+            print s, '[color=%s]'%c
 
-        for t in indirect_successors(all_dumps, s):
-            if successors(all_dumps, t):
-                print s,'->', t,'[style=dotted,arrowhead=open,color=gray]'
+        direct_edges = direct.get(s,set())
+        for t in direct_edges:
+            print s,'->', t,'[style=bold]'
 
-        for t in usage_successors(all_dumps, s):
-            if usage_successors(all_dumps, t):
+        for t in t_redux.get(s,set()):
+            if t not in direct_edges:
                 print s,'->', t,'[style=dashed,arrowhead=open,color=blue]'
     print '}'
 
