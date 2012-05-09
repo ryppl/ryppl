ba0fa15cabea92c387c2635f4f9310b173df8fa0
Dave Abrahams <dave@boostpro.com>
2012-05-09 19:22:36 -0400

    Add missing imports

--- ryppl/support/_zeroinstall.py
+++ ryppl/support/_zeroinstall.py
@@ -2,6 +2,8 @@
 # Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 from zeroinstall.injector import cli
+from subprocess import check_call
+import sys
 
 def launch(args, **kw):
     '''Effectively calls 0launch on with the given command-line
