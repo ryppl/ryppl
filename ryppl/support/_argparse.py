093ef76956f199c6842e705b3992a53e3cc8d216
Dave Abrahams <dave@boostpro.com>
2012-05-15 11:07:22 -0600

    Add command-line argument checker for 0install feed validity

--- ryppl/support/_argparse.py
+++ ryppl/support/_argparse.py
@@ -9,3 +9,11 @@ def existing_directory(dirname):
     if not os.path.isdir(dirname):
         raise ArgumentTypeError('%r: not a directory' % dirname)
     return dirname
+
+def valid_0install_feed(uri):
+    from zeroinstall.injector.model import canonical_iface_uri
+    from zeroinstall import SafeException
+    try:
+        return canonical_iface_uri(uri)
+    except SafeException, e:
+        raise ArgumentTypeError(e)
