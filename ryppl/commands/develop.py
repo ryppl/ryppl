d537dbf4d6a8564f50faf90830785047ff6b1e0c
Dave Abrahams <dave@boostpro.com>
2012-05-18 22:57:12 -0600

    Add SloppyCache to allow "develop" even when there are parts that can't be found via ZI feed
    Things like ICU or libbzip2 may already be installed and findable by CMake

--- ryppl/commands/develop.py
+++ ryppl/commands/develop.py
@@ -35,6 +35,46 @@ def command_line_interface(cli):
         , type=creatable_path
         , help='Path to project workspace directory, which must not already exist')
 
+from zeroinstall.injector import arch, model, iface_cache
+import platform
+uname = arch._uname + platform.uname()[len(arch._uname):]
+
+arch.machine_groups['missing'] = arch.machine_groups.get(uname[4], 0)
+arch.machine_ranks['missing'] = max(arch.machine_ranks.values()) + 1
+host_arch = '*-missing'
+
+class MissingImplementation(model.ZeroInstallImplementation):
+    # Assume that this (potential) binary is available so that we
+    # can select it as a dependency.
+    def is_available(self, stores):
+        return True
+
+class SloppyCache(iface_cache.IfaceCache):
+    def __init__(self):
+        iface_cache.IfaceCache.__init__(self)
+        self.done = set()
+
+    def get_feed(self, url, force = False):
+        feed = iface_cache.IfaceCache.get_feed(self, url, force)
+        if not feed: return None
+
+        if feed not in self.done:
+            self.done.add(feed)
+
+            # For each implementation, add a corresponding "missing"
+            # implementation
+
+            srcs = [x for x in feed.implementations.itervalues() if x.arch]
+            for x in srcs:
+                new_id = 'ryppl=' + x.id
+                if not new_id in feed.implementations:
+                    new = MissingImplementation(feed, new_id, None)
+                    feed.implementations[new_id] = new
+                    new.set_arch(host_arch)
+                    new.version = x.version
+
+        return feed
+
 def solve(args, config):
     selections = None
     versions = {}
@@ -124,9 +164,9 @@ def generate(args, selections, config):
         f.write(cmakelists_head)
     
     top_cmakelists_txt.write('''
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/.dependencies/ryppl/cmake/Modules")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/%s/ryppl/cmake/Modules")
 set(RYPPL_INITIAL_PASS TRUE CACHE BOOL "")
-''')
+''' % dependency_subdir)
     dep_cmakelists_txt.write(push_disable_tests_docs_examples)
 
     for uri,sel in selections.selections.items():
@@ -174,6 +214,8 @@ def run(args):
     os.environ['DISPLAY']=''
 
     config = zeroinstall.injector.config.load_config()
+    config._iface_cache = SloppyCache()
+
     # Only download new feed information every week unless otherwise
     # specified.  NOTE: Values lower than one hour will be ignored
     # unless you also monkeypatch
