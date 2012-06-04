76063dd24d6a393f5bb64de26f93ff52222619f0
Dave Abrahams <dave@boostpro.com>
2012-06-04 10:43:29 -0400

    Invoke CMake in path instead of letting zeroinstall get it
    Visual Studio / MSBuild chokes on CMake projects installed with
    ZeroInstall, apparently because of the "=" sign in the path
    names (again).  Therefore, we're having everyone install CMake
    themselves.

--- ryppl/support/cmake.py
+++ ryppl/support/cmake.py
@@ -1,20 +1,15 @@
 # Copyright Dave Abrahams 2012. Distributed under the Boost
 # Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-from ryppl.support import _zeroinstall
 from logging import info
 import subprocess
 import os
 
-cmake_2_8_8_plus = (
-    '--not-before=2.8.8'
-    , 'https://raw.github.com/ryppl/feeds/gh-pages/cmake.xml')
-
-command = _zeroinstall.launch_command + cmake_2_8_8_plus
+command = ('cmake',)
 
 def cmake(args, **kw):
-    info('cmake-2.8.8+ %s', args)
-    return _zeroinstall.launch(cmake_2_8_8_plus+args, **kw)
+    info('cmake %s', args)
+    return subprocess.check_call(command + tuple(args), **kw)
 
 def configure_for_circular_dependencies(*args, **kw):
     info('CMake first pass')
