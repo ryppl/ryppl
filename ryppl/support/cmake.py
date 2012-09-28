e96728559598459e388de1022ae81c35f946fe18
Dave Abrahams <dave@boostpro.com>
2012-09-28 18:29:04 -0400

    Revert "Invoke CMake in path instead of letting zeroinstall get it"
    This reverts commit 76063dd24d6a393f5bb64de26f93ff52222619f0.

--- ryppl/support/cmake.py
+++ ryppl/support/cmake.py
@@ -1,15 +1,20 @@
 # Copyright Dave Abrahams 2012. Distributed under the Boost
 # Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+from ryppl.support import _zeroinstall
 from logging import info
 import subprocess
 import os
 
-command = ('cmake',)
+cmake_2_8_8_plus = (
+    '--not-before=2.8.8'
+    , 'https://raw.github.com/ryppl/feeds/gh-pages/cmake.xml')
+
+command = _zeroinstall.launch_command + cmake_2_8_8_plus
 
 def cmake(args, **kw):
-    info('cmake %s', args)
-    return subprocess.check_call(command + tuple(args), **kw)
+    info('cmake-2.8.8+ %s', args)
+    return _zeroinstall.launch(cmake_2_8_8_plus+args, **kw)
 
 def configure_for_circular_dependencies(*args, **kw):
     info('CMake first pass')
